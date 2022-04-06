8e0f12336842276df36313924e03447870319aec
Kevin Jimenez <Kevin-0502>
2022-04-06 14:24:31 -0600

    bd update

---
+++ Controller/validaciones.php
@@ -0,0 +1,27 @@
+<?php
+function estaVacio($var){
+ return empty(trim($var));//si hay espacio en blanco
+}
+
+function esTexto($var){
+    return preg_match('/[a-zA-Z ]+$/',$var);
+}
+
+function esCodigo($var){
+    return preg_match('/^PROD[0]un{3}[0-9]$/',$var);
+}
+
+function esTelefono($var){
+    return preg_match('/^[267][0-9]{3}-?[0-9]{4}$/',$var);
+}
+
+function ComprobarImagen($archivo){
+    //La expresión regular analiza si el archivo es de
+    //una extensión válida para una imagen gif|jpeg|jpg|png
+    //utilizando la función preg_match()
+        $patron = "/\.(jpe?g|png)$/i";
+        $verificado = preg_match($patron, $archivo);
+        $esimagen = $verificado == true ? " (es imagen)" : " (no es imagen)";
+        return $esimagen;
+    }
+ ?>
---
+++ bd/textil_bd .sql
@@ -0,0 +1,175 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 06-04-2022 a las 22:22:59
+-- Versión del servidor: 10.4.22-MariaDB
+-- Versión de PHP: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `textil_bd`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `admins`
+--
+
+CREATE TABLE `admins` (
+  `correo` varchar(100) NOT NULL,
+  `nombres` varchar(100) NOT NULL,
+  `apellidos` varchar(100) NOT NULL,
+  `telefono` varchar(9) NOT NULL,
+  `direccion` varchar(500) NOT NULL,
+  `estado` varchar(10) NOT NULL,
+  `contraseña` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categorias`
+--
+
+CREATE TABLE `categorias` (
+  `id_categoria` varchar(20) NOT NULL,
+  `nombre` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `categorias`
+--
+
+INSERT INTO `categorias` (`id_categoria`, `nombre`) VALUES
+('C01', 'Textil'),
+('C02', 'Promocional');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `correo` varchar(100) NOT NULL,
+  `nombres` varchar(100) NOT NULL,
+  `apellidos` varchar(100) NOT NULL,
+  `telefono` varchar(9) NOT NULL,
+  `direccion` varchar(500) NOT NULL,
+  `numero_tarjeta` varchar(20) NOT NULL,
+  `estado` varchar(10) NOT NULL,
+  `contraseña` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `compras`
+--
+
+CREATE TABLE `compras` (
+  `id_compra` varchar(20) NOT NULL,
+  `correo` varchar(100) NOT NULL,
+  `fecha` date NOT NULL,
+  `monto_total` double NOT NULL,
+  `estado` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalles`
+--
+
+CREATE TABLE `detalles` (
+  `id_compra` varchar(20) NOT NULL,
+  `id_producto` varchar(20) NOT NULL,
+  `precio_unitario` double NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `estado` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `id_producto` varchar(20) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `descripcion` varchar(500) NOT NULL,
+  `imagen` varchar(100) NOT NULL,
+  `id_categoria` varchar(100) NOT NULL,
+  `precio` double NOT NULL,
+  `existencias` int(11) NOT NULL,
+  `estado` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`id_producto`, `nombre`, `descripcion`, `imagen`, `id_categoria`, `precio`, `existencias`, `estado`) VALUES
+('PROD001', 'Camisa tipo Polo', 'Camisa hecha a base de agodon ', '', '', 2.5, 300, '');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`correo`);
+
+--
+-- Indices de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`id_categoria`);
+
+--
+-- Indices de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`correo`),
+  ADD UNIQUE KEY `numero_tarjeta` (`numero_tarjeta`);
+
+--
+-- Indices de la tabla `compras`
+--
+ALTER TABLE `compras`
+  ADD PRIMARY KEY (`id_compra`),
+  ADD KEY `fk_cliente` (`correo`);
+
+--
+-- Indices de la tabla `detalles`
+--
+ALTER TABLE `detalles`
+  ADD PRIMARY KEY (`id_compra`,`id_producto`),
+  ADD KEY `fk_producto` (`id_producto`);
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`id_producto`),
+  ADD KEY `fk_categoria` (`id_categoria`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
